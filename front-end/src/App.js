0f150dc99f861647eb6995c8ab0af0b4387ba5e5
Felipe <fae.20@hotmail.com>
2019-03-28 22:22:41 -0300

    Implement Preloader Lines

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Lines } from 'react-preloaders';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <React.Fragment>
+        <Lines
+          color={'#f7f7f7'} 
+          bgColor={'#222'} 
+          time={2000} 
+        /> 
+      </React.Fragment>
     );
   }
 }
